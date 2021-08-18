7b39ae1d059ed22cc65953683fa87ff2f327d74a
isischameleon <isisdesade@gmail.com>
2021-08-18 11:16:11 +0000

    CSYS5030 week2

--- week1.py
+++ week1.py
@@ -1,6 +1,6 @@
 # Week 1
 
-from math import log2
+from math import log2, isclose
 import numpy as np
 import matplotlib.pyplot as plt
 from contextlib import suppress
@@ -13,7 +13,8 @@ def infocontent(p):
 def entropy1(p: np.array):
     if type(p) == list:
         p = np.array(p)
-    if np.sum(p) != 1:
+    # if np.sum(p) != 1:
+    if isclose(np.sum(p), 1, rel_tol=1e-6):
         raise Exception('The sum of the elements of p should be = 1: {}'.format(p))
     with suppress(ZeroDivisionError):
         H = - np.dot(p, np.where(p > 0, np.log2(p), 0))
@@ -22,8 +23,8 @@ def entropy1(p: np.array):
 
 # Calculate entropy of a sequence of stuff
 
- 
 def calculateDatasetShannonEntropy(items):
+    # also called empiricalentropy in course
     itemscount = collections.Counter(items)
     
     # probability = # item x / # total number of items
@@ -34,23 +35,21 @@ def calculateDatasetShannonEntropy(items):
  
     return entropy_value
 
+entropyempirical = lambda items : calculateDatasetShannonEntropy(items)
+
 def jointEntropy(p: np.array):
-    print(type(p))
-    print(p.shape[0])
-    print(p.shape[1])
+
     if type(p) == list:
         p = np.array(p)
-    if p.shape[0] != p.shape[1]:
-        raise Exception("p must be a square matrix")
-    print("np.sum(p, axis=0) != 1 :", np.sum(p, axis=0) != 1)
-    print("np.sum(p, axis=0):", np.sum(p, axis=0))
-    if ( np.sum(p, axis=0) != 1).any() or (np.sum(p, axis=1) != 1).any():
-        raise Exception('The sum of the elements of p should be = 1 in all dimensions {}'.format(p))
-    with suppress(ZeroDivisionError):
-        H = - np.dot(p, np.where(p > 0, np.log2(p), 0))
-        H = np.sum(H)
 
-    return  H
+    if not isclose(np.sum(p), 1, rel_tol=1e-6):
+        raise Exception('The sum of the elements of p should be = 1{}'.format(p))
+
+    # Perform element-wise p(x,y)*log p(x,y)
+    plogp = lambda x: - x * np.where(x > 0, np.log2(x), 0)
+    Hs = np.array([plogp(pij) for pij in p])
+
+    return  np.sum(Hs)
 
 def marginalP(p: np.array, dim: int):
     if dim + 1 > p.ndim:
@@ -61,20 +60,47 @@ marginalX = lambda  p : marginalP(p, 0)
 
 marginalY = lambda  p : marginalP(p, 1)
 
-def entropyXGivenY(p: np.array):
+def conditionalentropy2(p: np.array):
+    # entropyXGivenY
+    # H(X|Y)
     # 2 dim only for now
 
     # e.g.
     # p(x,y)
-    # Y rows (dim=0) \X   columns (dim=1)
+    # coded for Y rows (dim=0) \X   columns (dim=1)
+    # =>
+    # transpose(p)
     # p = np.array([[0.125, 0.0625, 0.03125, 0.03125],
     #       [0.0625, 0.125, 0.03125, 0.03125],
     #       [0.0625, 0.0625, 0.0625, 0.0625],
     #       [0.25,0,0,0]])
-    # Correct answer = 11/8 = 1.375
+    # Correct answer = 11/8 = 1.375 fro transpose p                    
+    if (type(p) == list):
+        p = np.array(p)
+
+    p = np.transpose(p)
+
     total = 0
     for i in range(p.shape[0]):
         pyi = np.sum(p[i])
         total+= pyi * entropy1(1/pyi * p[i])
 
     return total
+
+def conditionalentropy(p: np.array):
+    # entropyXGivenY
+    # H(X|Y)
+    # 2 dim only for now
+    # X = rows
+    # Y = columns
+                  
+    if (type(p) == list):
+        p = np.array(p)
+
+    total = 0
+    for i in range(p.shape[1]):
+        pxi = np.sum(p[:,i])
+        total+= pxi * entropy1(1/pxi * p[:,i])
+
+    return total
+
