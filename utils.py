02cd5d2eda0aea8e5e8008d12159dc731bd227c5
isischameleon <isisdesade@gmail.com>
2021-10-20 05:50:57 +0000

    Week10 Before class

--- utils.py
+++ utils.py
@@ -17,15 +17,28 @@ plt.show()
 # START JVM
 #-----------------------------------------------------
 
-from jpype import startJVM, isJVMStarted, getDefaultJVMPath, JArray, JInt, JPackage, shutdownJVM, JDouble
-import numpy
+from jpype import startJVM, getDefaultJVMPath, JArray, JDouble, JInt, JPackage, shutdownJVM, isJVMStarted
+import numpy as np
 import sys
 # Our python data file readers are a bit of a hack, python users will do better on this:
 sys.path.append("/home/jovyan/notebooks/jidt/demos/python")
-import readIntsFile, readFloatsFile
+import readIntsFile
 
 # Add JIDT jar library to the path
 jarLocation = "/home/jovyan/notebooks/jidt/infodynamics.jar"
 # Start the JVM (add the "-Xmx" option with say 1024M if you get crashes due to not enough memory space)
 if (not isJVMStarted()):
     startJVM(getDefaultJVMPath(), "-ea", "-Djava.class.path=" + jarLocation)
+
+javaIntList = lambda l : JArray(JInt, 1)(l)
+javaDoubleList = lambda l : JArray(JDouble, 1)(l)
+
+# LOAD DATA FILE
+#-------------------
+
+import pandas as pd
+#from pathlib import Path
+
+filename = './data/SFI-heartRate_breathVol_bloodOx-extract.txt'
+data = pd.read_csv(filename, delim_whitespace=True, comment='%', header=None, names = ['Heart rate', 'Chest volume', 'Blood oxygen'], index_col=None)
+data.head()
